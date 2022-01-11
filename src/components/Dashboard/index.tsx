16ce7fa430d465273b601da986f73053ed1e8e8d
Gildinei S. Anunciação <gildineisa@gmail.com>
2022-01-11 13:46:38 -0300

    Update repository

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summay";
+import { Container } from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Summary />
+        </Container>
+    );
+};
