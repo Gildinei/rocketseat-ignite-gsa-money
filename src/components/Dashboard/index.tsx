ebb5afdfba7cfffd5447e84fda68d72159d29a3e
Gildinei S. Anunciação <gildineisa@gmail.com>
2022-01-12 13:41:29 -0300

    Adiciona Dashboard e Tabela de transações

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import { Summary } from "../Summay";
+import { TransactionsTable } from "../TransactionsTable";
+
 import { Container } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionsTable />
         </Container>
     );
 };
