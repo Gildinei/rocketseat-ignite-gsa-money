9f588801c9df26f2c6a9e07fabb3062b74741e57
Gildinei S. Anunciação <gildineisa@gmail.com>
2022-03-03 23:09:08 -0300

    Feat: adiciona listagem de transações na tabela

---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,65 @@
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react';
+import { api } from '../services/api';
+
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+};
+
+// interface TransactionInput {
+//     title: string;
+//     amount: number;
+//     type: string;
+//     category: string;
+// };
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+interface TransactionsProviderProps {
+    children: ReactNode;
+};
+
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (Transaction: TransactionInput) => Promise<void>;
+};
+
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    useEffect(() => {
+        api.get('transactions').then(response => setTransactions(response.data.transactions));
+    }, []);
+
+    async function createTransaction(transactionInput: TransactionInput) {
+        const responde = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        });
+        
+        const { transaction } = responde.data;
+
+        setTransactions([
+            ...transactions,
+            transaction
+        ]);
+    };
+
+    return (
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+            {children}
+        </TransactionsContext.Provider>
+    );
+};
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
+};
